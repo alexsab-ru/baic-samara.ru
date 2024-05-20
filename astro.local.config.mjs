20197c78974c0b35ae16fe47fd94f38771379825
Alexander Sabanov <alexsab@mail.ru>
2024-05-20 21:38:42 +0400

    upd astro config

--- astro.local.config.mjs
+++ astro.local.config.mjs
@@ -10,7 +10,7 @@ import icon from "astro-icon";
 export default defineConfig({
 	integrations: [
 		tailwind({
-			configFile: './tailwind.baic-samara.js'
+			configFile: './tailwind.baic.js'
 		}),
 		sitemap(),
 		robots(),
